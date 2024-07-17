5673f330d1488d07bdfc6f6885fdb721c99dac77
Tamiris Cristine <tamiris.css@gmail.com>
2024-07-17 15:01:10 +0200

    feat: add Toast alert

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Toaster } from 'sonner'
 import Sidebar from './components/Sidebar'
 import Tasks from './components/Tasks'
 
 const App = () => {
   return (
     <div className="flex">
+      <Toaster
+        toastOptions={{
+          style: {
+            color: '#35383E',
+          },
+        }}
+      />
       <Sidebar />
       <Tasks />
     </div>
--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -8,6 +8,7 @@ import TasksSeparator from './TasksSeparator'
 import { useState } from 'react'
 import TASKS from '../assets/constants/tasks'
 import TaskItem from './TaskItem'
+import { toast } from 'sonner'
 
 const Tasks = () => {
   const [tasks, setTasks] = useState(TASKS)
@@ -19,15 +20,24 @@ const Tasks = () => {
   const handleTaskDeleteClick = (taskId) => {
     const newTasks = tasks.filter((task) => task.id !== taskId)
     setTasks(newTasks)
+    toast.success('Tarefa deletada com sucesso!')
   }
 
   const handleTaskCheckboxClick = (taskId) => {
     const newTasks = tasks.map((task) => {
       if (task.id !== taskId) return task
-      if (task.status === 'not_started')
+      if (task.status === 'not_started') {
+        toast.success('Tarefa iniciada com sucesso!')
         return { ...task, status: 'in_progress' }
-      if (task.status === 'in_progress') return { ...task, status: 'done' }
-      if (task.status === 'done') return { ...task, status: 'not_started' }
+      }
+      if (task.status === 'in_progress') {
+        toast.success('Tarefa concluída com sucesso!')
+        return { ...task, status: 'done' }
+      }
+      if (task.status === 'done') {
+        toast.success('Tarefa reiniciada com sucesso!')
+        return { ...task, status: 'not_started' }
+      }
 
       return task
     })
