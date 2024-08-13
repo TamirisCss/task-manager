4ae79f1154dc62d1c59852c054eb43a4d7c675b7
Tamiris Cristine <tamiris.css@gmail.com>
2024-08-13 11:27:05 +0200

    refactor: add tailwind-variants to Button component

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -65,7 +65,7 @@ const Tasks = () => {
         </div>
 
         <div className="flex items-center gap-3">
-          <Button variant="ghost">
+          <Button color="ghost">
             Limpar Tarega <TrashIcon />{' '}
           </Button>
           <Button onClick={() => setAddTaskDialogIsOpen(true)}>
