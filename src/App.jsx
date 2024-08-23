e4d9bf21ab24e1958f334534e7e9cf6fd6b0edaf
Tamiris Cristine <tamiris.css@gmail.com>
2024-08-23 11:11:13 +0200

    feat: ensure task can be deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,9 @@
-import { Toaster } from 'sonner'
-
 import Sidebar from './components/Sidebar'
 import Tasks from './components/Tasks'
 
 const App = () => {
   return (
     <div className="flex">
-      <Toaster
-        toastOptions={{
-          style: {
-            color: '#35383E',
-          },
-        }}
-      />
       <Sidebar />
       <Tasks />
     </div>
